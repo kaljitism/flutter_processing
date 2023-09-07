57b22f48b66b57e19867e752f1db40534f871d15
Aditya Dwivedi <dwikaljit@gmail.com>
2023-09-07 05:53:52 +0530

    wrote basic Processing and Sketch APIs to implement background functionality.

---
+++ lib/src/_core.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/widgets.dart';
+
+class Processing extends StatelessWidget {
+  const Processing({
+    super.key,
+    required this.sketch,
+  });
+
+  final Sketch sketch;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size.infinite,
+      painter: _SketchPainter(sketch: sketch),
+    );
+  }
+}
+
+abstract class Sketch {
+  void setup();
+
+  void draw();
+
+  Canvas? canvas;
+  Size? size;
+
+  void background({
+    required Color color,
+    required Size size,
+  }) {
+    final paint = Paint()..color = color;
+    canvas?.drawRect(Offset.zero & size, paint);
+  }
+}
+
+class _SketchPainter extends CustomPainter {
+  final Sketch sketch;
+
+  _SketchPainter({super.repaint, required this.sketch});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    sketch
+      ..canvas = canvas
+      ..size = size
+      ..setup()
+      ..draw();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
---
+++ test/core_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('core', () {
+    testWidgets(
+        "User can paint through processing Widget", (widgetTester) async {});
+  });
+}
