17b5b684abc3b15bb2f50fbfbef9a7b88a6ef7f0
Aditya Dwivedi <dwikaljit@gmail.com>
2023-09-09 16:19:59 +0530

    wrote check for background in all possible scenarios.

--- test/core_test.dart
+++ test/core_test.dart
@@ -5,22 +5,49 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   group('core', () {
-    testGoldens("User can paint through processing Widget",
-        (widgetTester) async {
+    testGoldens("User can paint background in setup()", (widgetTester) async {
       widgetTester.view
         ..physicalSize = const Size(100, 100)
         ..devicePixelRatio = 1.0;
 
       await widgetTester.pumpWidget(
         Processing(
-          sketch: MyTestSketch(),
+          sketch: PaintBackgroundInSetupSketch(),
         ),
       );
 
-      await screenMatchesGolden(widgetTester, "core_draw_background");
+      await screenMatchesGolden(widgetTester, "core_background_setup");
     });
 
-    testGoldens("User can paint through processing Widget",
+    testGoldens("User can paint background in draw()", (widgetTester) async {
+      widgetTester.view
+        ..physicalSize = const Size(100, 100)
+        ..devicePixelRatio = 1.0;
+
+      await widgetTester.pumpWidget(
+        Processing(
+          sketch: PaintBackgroundInDrawSketch(),
+        ),
+      );
+
+      await screenMatchesGolden(widgetTester, "core_background_draw");
+    });
+
+    testGoldens("User can paint background in orange", (widgetTester) async {
+      widgetTester.view
+        ..physicalSize = const Size(100, 100)
+        ..devicePixelRatio = 1.0;
+
+      await widgetTester.pumpWidget(
+        Processing(
+          sketch: PaintOrangeBackgroundSketch(),
+        ),
+      );
+
+      await screenMatchesGolden(widgetTester, "core_background_orange");
+    });
+
+    testGoldens("background in draw() replaces background in setup()",
         (widgetTester) async {
       widgetTester.view
         ..physicalSize = const Size(100, 100)
@@ -28,20 +55,54 @@ void main() {
 
       await widgetTester.pumpWidget(
         Processing(
-          sketch: MyTestSketch(),
+          sketch: PaintBackgroundInSetupAndDrawSketch(),
         ),
       );
 
-      await screenMatchesGolden(widgetTester, "core_draw_background");
+      await screenMatchesGolden(widgetTester, "core_background_setup_draw");
     });
   });
 }
 
-class MyTestSketch extends Sketch {
+class PaintBackgroundInSetupSketch extends Sketch {
+  @override
+  void setup() {
+    background(
+      color: const Color(0xff404040),
+    );
+  }
+}
+
+class PaintBackgroundInDrawSketch extends Sketch {
   @override
   void draw() {
     background(
-      color: const Color(0xffFFF237),
+      color: const Color(0xff000000),
+    );
+  }
+}
+
+class PaintOrangeBackgroundSketch extends Sketch {
+  @override
+  void draw() {
+    background(
+      color: const Color(0xffFFCC00),
+    );
+  }
+}
+
+class PaintBackgroundInSetupAndDrawSketch extends Sketch {
+  @override
+  void setup() {
+    background(
+      color: const Color(0xffffcc00),
+    );
+  }
+
+  @override
+  void draw() {
+    background(
+      color: const Color(0xff000000),
     );
   }
 }
